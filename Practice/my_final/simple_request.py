fa25f7615492d5768ab0086f2446a0433dfe542a
Kamo Petrosyan <kamo@haikson.com>
2020-08-17 10:20:20 +0300

    request

--- Practice/my_final/simple_request.py
+++ Practice/my_final/simple_request.py
@@ -5,10 +5,8 @@ import urllib.request
 import json
 
 
-def get_prediction(description, company_profile, benefits):
-    body = {'description': description,
-                            'company_profile': company_profile,
-                            'benefits': benefits}
+def get_prediction(**kwargs):
+    body = kwargs
 
     myurl = "http://localhost:5000/predict"
     req = urllib.request.Request(myurl)
@@ -21,8 +19,9 @@ def get_prediction(description, company_profile, benefits):
     return json.loads(response.read())['predictions']
 
 if __name__=='__main__':
-    description = '''Stylect is a dynamic startup that helps helps women discover and buy shoes. We’re a small team based in London that has previously worked at Google, Techstars, Pixelmator and Rocket Internet.We place a high premium on simplicity no matter what we’re working on (i.e. design, programming, marketing). We’re also a team that ships fast. We built version 1 of our app in a week, the next release (built in a month) was featured in the Apple Appstore Italy as a best new fashion app. Fast release cycles are challenging, but also very fun - which is why we love them.\xa0As we’ve grown, the projects that we’re working on have grown both in scale and in technical complexity. \xa0Stylect is looking for someone who can help us improve our backend which gathers product data; analyses/categorizes it; and shows it to thousands of users daily. Each step in the process has unique challenges that demands a strong technical background.\xa0'''
-    company_profile = '''None'''
-    benefits = '''We are negotiable on salary and there is the potential for equity for the right candidate.'''
-    preds = get_prediction(description, company_profile, benefits)
+    data = {
+        "comment": "Good thing Soros is so rich, these guys must be making a killing on overtime.",
+        "subreddit": "entertainment",
+        "parent_comment": "What is that"}
+    preds = get_prediction(**data)
     print(preds)
